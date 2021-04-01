1d86f99f88c75f766e7f28fbd9112aaa9e945486
burger-okd <ok9623@gmail.com>
2021-04-02 00:44:28 +0900

    add printing methods

--- review.py
+++ review.py
@@ -1,10 +1,22 @@
 # Getting user input
-score = int(input("what's the score(0-100):"))
+math_score = int(input("What's your math score(0-100): "))
+physics_score = int(input("What's your pyhsics socre(0-100): "))
+comment = str(input("Anything want to metion? "))
 
 # types of print
-print("Do not break the lines")
-print("")
-print('score:', score)#, end = '')
+print('math_score:',math_score, end = ' ,')
+print('physics_score:', physics_score)
+print(comment)
+
+print('This is string :', comment, 'This is number :' ,math_score, physics_score)
+
+print('This is string :' + comment, 'This is number :', math_score + physics_score)
+
+#The way to print multiple items
+print('{1} and {0}'.format('spam', 'eggs'))
+str1='str1'; num1=1; num2=2; num3=3; num4=4
+print('{0} {1} %d {2} {3} %s'.format('test','11',num1,num2)%(num3,str1))
+#
 
 
 import time
