d3abda0f34332707960ee4aaeec28fdd6186651d
burger-okd <ok9623@gmail.com>
2021-04-02 00:31:34 +0900

    add review.py file

---
+++ review.py
@@ -0,0 +1,17 @@
+# Getting user input
+score = int(input("what's the score(0-100):"))
+
+# types of print
+print("Do not break the lines")
+print("")
+print('score:', score)#, end = '')
+
+
+import time
+# store start time 
+start = time.time()
+
+# Running codes
+
+# Calculate the running time
+print("time: ", time.time() -start)
